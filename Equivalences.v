4ef26d4210481a1b75c9b3229a52dedeea14ca68
Clara de Andrade <clara.andrade@protonmail.com>
2024-08-20 19:15:06 -0300

    Elementary theorems about qinv and isequiv.

--- Equivalences.v
+++ Equivalences.v
@@ -115,41 +115,169 @@ Defined.
 Definition qinv {A B : Type} (f : A -> B) : Type
   := ∑ (g : B -> A), ((g ∘ f ~ id) × (f ∘ g ~ id)).
 
+
+Definition qinv_to_op {A B : Type} (f : A -> B) : qinv f -> B -> A :=
+  fun e => fst e.
+Notation "f \'" := (qinv_to_op f _)
+  (at level 3)
+  : core_scope.
+
+
+Example qinv_id {A : Type} : qinv (@id A).
+Proof.
+  unfold qinv.
+  exists id. split.
+  - reflexivity.
+  - reflexivity.
+Defined.
+
+Example qinv_qinv {A B : Type} (f : A -> B)
+  (e : qinv f) : qinv (fst e).
+Proof.
+  destruct e as [g [alph beta]].
+  unfold qinv. unfold fst.
+  exists f. split.
+  - exact beta.
+  - exact alph.
+Defined.
+
+Example qinv_comp {A B C : Type} (f : A -> B) (g : B -> C)
+  : qinv f -> qinv g -> qinv (g ∘ f).
+Proof.
+  intros e1 e2.
+  destruct e1 as [f' p1].
+  destruct e2 as [g' p2].
+  destruct p1 as [alph beta].
+  destruct p2 as [gamm delt].
+  exists (f' ∘ g').
+  split.
+  - assert (eta : g' ∘ g ∘ f ~ f).
+    {
+      rewrite <- id_unit_l.
+      apply homotopy_comp_r.
+      exact gamm.
+    }
+    apply homotopy_tr with (g := f'∘ f).
+    + rewrite <- comp_assoc.
+      apply homotopy_comp_l.
+      exact eta.
+    + exact alph.
+  - assert (eta : g ∘ (f ∘ f') ~ g).
+    {
+      rewrite <- id_unit_r.
+      apply homotopy_comp_l.
+      exact beta.
+    }
+    apply homotopy_tr with (g := g ∘ g').
+    + rewrite -> comp_assoc.
+      apply homotopy_comp_r.
+      exact eta.
+    + exact delt.
+Defined.
+
+
 Definition isequiv {A B : Type} (f : A -> B) : Type
   := (∑ (g : B -> A), (g ∘ f ~ id)) × (∑ (g : B -> A), (f ∘ g ~ id)).
 
-Definition homotopy_eqv (A B : Type) : Type :=
+Lemma qinv_to_isequiv {A B : Type} (f : A -> B) : qinv f -> isequiv f.
+Proof.
+  intros e.
+  destruct e as [g p].
+  destruct p as [alph beta].
+  split.
+  + refine (g, alph).
+  + refine (g, beta).
+Defined.
+
+Lemma isequiv_to_qinv {A B : Type} (f : A -> B) : isequiv f ->  qinv f.
+Proof.
+  intros e.
+  destruct e as [p q].
+  destruct p as [g alph].
+  destruct q as [h beta].
+  exists g. split.
+  + refine alph.
+  + assert (gamm: g ~ h).
+    {
+      apply homotopy_eucl_l with (g ∘ f ∘ h).
+      - rewrite <- id_unit_r.
+        rewrite <- comp_assoc.
+        apply homotopy_comp_l.
+        refine beta.
+      - rewrite <- id_unit_l.
+        apply homotopy_comp_r.
+        refine alph.
+    }
+    refine (fun x => (ap f (gamm x)) • (beta x)).
+Defined.
+
+Lemma qinv_iff_isequiv {A B : Type} (f : A -> B) : qinv f <-> isequiv f.
+Proof.
+  split.
+  - apply qinv_to_isequiv.
+  - apply isequiv_to_qinv.
+Defined.
+
+
+Definition homeqv (A B : Type) : Type :=
   ∑ (f : A -> B), isequiv f.
-Infix "≃" := homotopy_eqv
+Infix "≃" := homeqv
   : type_scope.
 
 
-Lemma qinv_logeqv_isequiv {A B : Type} (f : A -> B) :
-  qinv f <-> isequiv f.
+Example homeqv_ref (A : Type) : A ≃ A.
 Proof.
-  split.
-  - intros e.
-    destruct e as [g p].
-    destruct p as [alph beta].
-    split.
-    + refine (g, alph).
-    + refine (g, beta).
-  - intros e.
-    destruct e as [p q].
-    destruct p as [g alph].
-    destruct q as [h beta].
-    exists g. split.
-    + refine alph.
-    + assert (gamm: g ~ h).
-      {
-        apply homotopy_eucl_l with (g ∘ f ∘ h).
-        - rewrite <- id_unit_r.
-          rewrite <- comp_assoc.
-          apply homotopy_comp_l.
-          refine beta.
-        - rewrite <- id_unit_l.
-          apply homotopy_comp_r.
-          refine alph.
-      }
-      refine (fun x => (ap f (gamm x)) • (beta x)).
-Qed.
+  exists id.
+  apply qinv_to_isequiv.
+  exact qinv_id.
+Defined.
+
+Instance homeqv_reflexive
+  : Reflexive homeqv.
+Proof.
+  exact homeqv_ref.
+Defined.
+
+
+Example homeqv_sym (A B : Type) : A ≃ B -> B ≃ A.
+Proof.
+  intros e.
+  destruct e as [f p].
+  apply isequiv_to_qinv in p.
+
+  unfold homeqv.
+  exists (fst p).
+  apply qinv_to_isequiv.
+
+  apply qinv_qinv.
+Defined.
+
+Instance homeqv_symmetric
+  : Symmetric homeqv.
+Proof.
+  exact homeqv_sym.
+Defined.
+
+
+Example homeqv_tr (A B C : Type) : A ≃ B -> B ≃ C -> A ≃ C.
+Proof.
+  intros e1 e2.
+  destruct e1 as [f p1].
+  apply isequiv_to_qinv in p1.
+  destruct e2 as [g p2].
+  apply isequiv_to_qinv in p2.
+
+  unfold homeqv.
+  exists (g ∘ f).
+  apply qinv_to_isequiv.
+
+  apply qinv_comp.
+  - exact p1.
+  - exact p2.
+Defined.
+
+Instance homeqv_transitive
+  : Transitive homeqv.
+Proof.
+  exact homeqv_tr.
+Defined.
